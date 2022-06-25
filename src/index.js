75c6dfae796ad214abc466d78b5b66f6c7a7366a
kirillsankov <73188231+kirillsankov@users.noreply.github.com>
2022-06-25 16:50:45 +0300

    [orientation] Добавил скрипт подъема блоков, сверстал секцию "Дети и взрослые"

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import './styles/index.scss';
 import 'bootstrap';
 import Example from "./scripts/script";
 import Slider from "./scripts/slider";
+import Box from "./scripts/box";
 
+const Items = document.querySelectorAll('.box__item');
+new Box(Items);
 [...document.querySelectorAll('.swiper')].forEach((elem) => new Slider(elem));
