91fdfdfe11e868fd7129e4c368963745863aedbf
qekex <dimanys-12032001@mail.ru>
2022-06-26 05:28:50 +0300

    [develop] порефакторил блоки верстки, вынес кнопку в бутстрап

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import './styles/index.scss';
 import 'bootstrap';
-import Example from "./scripts/script";
 import Slider from "./scripts/slider";
-import Box from "./scripts/box";
 
-const Items = document.querySelectorAll('.box__item');
-new Box(Items);
 [...document.querySelectorAll('.swiper')].forEach((elem) => new Slider(elem));
